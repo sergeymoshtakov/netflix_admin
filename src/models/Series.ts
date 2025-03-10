938d9990401009530dc8e0856e0088f311386c1d
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2025-03-11 00:57:04 +0100

    added category model

--- src/models/Series.ts
+++ src/models/Series.ts
@@ -1,16 +1,27 @@
 export interface Episode {
-    id: number;
-    title: string;
-    description: string;
-    duration: number;
-    releaseDate: string;
-  }
+  id: number;
+  title: string;
+  description: string;
+  duration: number;
+  releaseDate: string;
+}
   
-  export interface Series {
-    id: number;
-    title: string;
-    description: string;
-    releaseDate: string;
-    rating: number;
-    episodes: Episode[];
-  }
+export interface Series {
+  id: number;
+  title: string;
+  description: string;
+  releaseDate: string;
+  rating: number;
+  episodes: Episode[];
+}
+
+export interface Category{
+  id: number;
+  title: string;
+}
+
+export interface SeriesCategory{
+  id: number;
+  series_id: number;
+  category_id: number;
+}
