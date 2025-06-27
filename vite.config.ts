7d2a62979b51f61d7b82b0a855051df2aa9e94e1
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2025-06-27 12:55:49 +0200

    added routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/genres': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/appUsers': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/contents': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/roles': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/contentTypes': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/actors': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/episodes': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/warnings': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+});
