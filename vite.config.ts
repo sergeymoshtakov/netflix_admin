39fb6bae9757adb34ba99bee53cd71916fe94fa4
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2025-07-12 09:55:19 +0200

    deleted proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const backendTarget = 'http://cinemate.ddns.net:8081';
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: Object.fromEntries([
-      '/api/v1/auth/login',
-      '/api/v1/auth/logout',
-      '/api/v1/roles',
-      '/api/v1/users',
-      '/api/v1/users/add',
-      '/api/v1/actors/all',
-      '/api/v1/actors',
-      '/api/v1/genres/all',
-      '/api/v1/genres',
-      '/api/v1/warnings/all',
-      '/api/v1/warnings',
-      '/api/v1/content-types/all',
-      '/api/v1/content-types',
-      '/api/v1/admin/contents',
-      '/api/v1/admin/episodes/',
-      '/appUsers',
-      '/roles',
-      '/userRoles',
-      '/episodes',
-      '/trailers',
-    ].map((path) => [
-      path,
-      {
-        target: backendTarget,
-        changeOrigin: true,
-      },
-    ])),
-  },
 });
